d64350ad0ef134adc18802729038c47465bbb1db
ampueropierre <ampuero.pierre@gmail.com>
2018-05-28 15:52:45 +0200

    model devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :items
+  devise_for :users
+  root 'home#index'
   get 'home/shop'
   get 'home/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
