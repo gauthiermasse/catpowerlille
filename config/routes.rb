f713b5a7cf0c2545ccbc3739e3f9fb3dfdcf1e01
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-28 15:39:11 +0200

    image background

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
   get 'home/shop'
   get 'home/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
