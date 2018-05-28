af2e6f9ae08a9bdcb7c6a8462c7e31aeee03972e
guillaume <g.delory62@gmail.com>
2018-05-28 17:07:01 +0200

    ajout shop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :items
   devise_for :users
   root 'home#index'
-  get 'home/shop'
+  get 'home/shop', to: 'home#shop'
   get 'home/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
