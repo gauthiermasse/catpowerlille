c84d89d4aca5029e14d546b84e82445f2bc1af02
ampueropierre <ampuero.pierre@gmail.com>
2018-05-29 13:57:02 +0200

    Add order and cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
-<<<<<<< HEAD
-=======
-	has_and_belongs_to_many :users
->>>>>>> af2e6f9ae08a9bdcb7c6a8462c7e31aeee03972e
+	has_and_belongs_to_many :carts
+	has_and_belongs_to_many :orders
+
 end
