94a851ff13b711d29feca12fb45f34ed17daeea7
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-29 15:08:09 +0200

    réedition des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :users 
+  
   resources :items
   devise_for :users
   root 'home#index'
   get 'home/shop', to: 'home#shop'
   get 'home/contact'
   get 'home/panier', to: 'home#panier'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+
 end
