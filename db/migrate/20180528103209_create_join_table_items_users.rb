01ba3689f2ec546443e0c5ebb23191fa6cc8a93c
ampueropierre <ampuero.pierre@gmail.com>
2018-05-29 11:17:10 +0200

    Delete scaffolds.scss

--- db/migrate/20180528103209_create_join_table_items_users.rb
+++ db/migrate/20180528103209_create_join_table_items_users.rb
@@ -1,8 +1,3 @@
 class CreateJoinTableItemsUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :items, :users do |t|
-       t.index [:item_id, :user_id]
-       t.index [:user_id, :item_id]
-    end
-  end
+  
 end
