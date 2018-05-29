51ca4b3cd89e48da53dd122d284d6a7774700daa
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-29 15:23:42 +0200

    photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
   def show
   	#@user = User.find(params[:id])
   	#@email = user.email
+	 #@user = User.all
+  end
+ def showid
+  	#@user = User.find(params[:id])
 
   end
+
 end
