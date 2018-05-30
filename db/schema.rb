414e47533b7925842989758c5e1409b46bc04eae
ampueropierre <ampuero.pierre@gmail.com>
2018-05-30 16:56:44 +0200

    change notice and alert

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_05_30_134334) do
 
   create_table "carts", force: :cascade do |t|
