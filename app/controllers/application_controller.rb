5e166fbeddc9057daf8f26beac57fea9ac23d319
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-30 16:35:46 +0200

    AddfullnameToUser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,10 @@ protect_from_forgery with: :exception
 before_action :configure_permitted_parameters, if: :devise_controller?
 
 protected
-def configure_permitted_parameters
+	
+	def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
 
-
+	end
 end
