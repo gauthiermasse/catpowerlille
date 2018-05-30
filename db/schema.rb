d38fc65b0915affa258582b2fc090ad708c01ac8
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-30 16:00:00 +0200

    Création du champ fullname ds la table User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_095327) do
+ActiveRecord::Schema.define(version: 2018_05_30_134334) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2018_05_30_095327) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
