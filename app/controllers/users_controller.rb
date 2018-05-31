93a97c8b3c135c31946562c2e9dc49bcab8ae331
guillaume <g.delory62@gmail.com>
2018-05-31 18:01:30 +0200

    essai stripe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   def show
-  	p 'voila'
   	@orders = Order.find_by_user_id(current_user.id)
-  	p @orders
   	@price = 0
   end
- 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   root 'home#index'
-  get 'home/shop', to: 'home#shop', as: 'shop'
-  get 'home/contact'
-  get 'home/panier', to: 'home#panier', as: 'panier'
-  get 'users/show', to: 'users#show'
+  get 'shop', to: 'home#shop', as: 'shop'
+  get 'contact', to: 'home#contact', as: 'home_contact'
+  get 'panier', to: 'home#panier', as: 'panier'
+  post 'panier', to: 'home#pay'
+  get 'profil', to: 'users#show', as: 'users_show'
   get 'add:id', to: 'home#add', as: 'add'
   get 'remove:id', to: 'home#remove', as:'remove'
-  get 'pay', to: 'home#pay', as: 'pay'
-  post 'pay', to: 'home#pay'
+  post 'pay', to: 'home#pay', as: 'pay'
   get 'orders', to: 'home#orders'
 end
